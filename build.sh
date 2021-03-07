627195de406beeb44b0da9c19949ee92da497b6b
htchepannou <htchepannou@expedia.com>
2021-03-07 14:59:44 -0500

    add deployment to s3

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit if any command failed
+set -e
+
+# Clean
+rm -Rf target
+mkdir target
+
+# Build
+for file in $(find . -type f -iname '*_api.yaml'); do
+  filename=$(basename $file suffix)
+
+  swagger-cli validate $file
+  swagger-cli bundle -t yaml -o target/$filename $file
+done
