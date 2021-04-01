fc2875054be3bb7afe1bbbf2ab301f3173ba100b
htchepannou <htchepannou@expedia.com>
2021-04-01 17:46:00 -0400

    add comments

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ mkdir target
 for file in $(find . -type f -iname '*_api.yaml'); do
   filename=$(basename $file suffix)
 
+  echo ">>> Validating $file"
   swagger-cli validate $file
+
+  echo ">>> Bundling $file to target/$filename"
   swagger-cli bundle -t yaml -o target/$filename $file
 done
